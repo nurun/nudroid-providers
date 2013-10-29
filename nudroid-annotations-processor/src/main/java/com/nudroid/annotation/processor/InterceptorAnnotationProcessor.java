b88d85a9ebf356c9ba1afa4b906a2fced3cab1a2
Daniel Freitas <daniel.mfreitas@gmail.com>
2013-10-29 09:21:14 -0400

    Moved concrete annotation creation logic to interceptor processor

--- nudroid-annotations-processor/src/main/java/com/nudroid/annotation/processor/InterceptorAnnotationProcessor.java
+++ nudroid-annotations-processor/src/main/java/com/nudroid/annotation/processor/InterceptorAnnotationProcessor.java
@@ -9,8 +9,11 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.ArrayType;
+import javax.lang.model.type.TypeKind;
 import javax.lang.model.util.ElementFilter;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
@@ -31,145 +34,260 @@ import com.nudroid.annotation.provider.interceptor.ProviderInterceptorPoint;
  */
 class InterceptorAnnotationProcessor {
 
-    private LoggingUtils mLogger;
-    private Types mTypeUtils;
-    private Elements mElementUtils;
+	private LoggingUtils mLogger;
+	private Types mTypeUtils;
+	private Elements mElementUtils;
 
-    /**
-     * Creates an instance of this class.
-     * 
-     * @param processorContext
-     *            The processor context parameter object.
-     */
-    InterceptorAnnotationProcessor(ProcessorContext processorContext) {
+	/**
+	 * Creates an instance of this class.
+	 * 
+	 * @param processorContext
+	 *            The processor context parameter object.
+	 */
+	InterceptorAnnotationProcessor(ProcessorContext processorContext) {
 
-        this.mLogger = processorContext.logger;
-        this.mTypeUtils = processorContext.typeUtils;
-        this.mElementUtils = processorContext.elementUtils;
-    }
+		this.mLogger = processorContext.logger;
+		this.mTypeUtils = processorContext.typeUtils;
+		this.mElementUtils = processorContext.elementUtils;
+	}
 
-    /**
-     * Process the {@link Query} annotations on this round.
-     * 
-     * @param roundEnv
-     *            The round environment to process.
-     * @param metadata
-     *            The annotation metadata for the processor.
-     * @param continuation
-     *            The continuation object for this processor.
-     */
-    void process(RoundEnvironment roundEnv, Metadata metadata, Continuation continuation) {
+	/**
+	 * Process the {@link Query} annotations on this round.
+	 * 
+	 * @param roundEnv
+	 *            The round environment to process.
+	 * @param metadata
+	 *            The annotation metadata for the processor.
+	 * @param continuation
+	 *            The continuation object for this processor.
+	 */
+	void process(RoundEnvironment roundEnv, Metadata metadata, Continuation continuation) {
 
-        /*
-         * Do not assume that because the @ProviderInterceptorPoint annotation can only be applied to annotation types,
-         * only TypeElements will be returned. Compilation errors on a class can let the compiler think the annotation
-         * is applied to other elements even if it is correctly applied to a class, causing a class cast exception in
-         * the for loop below.
-         */
-        Set<Element> interceptorAnnotations = new HashSet<Element>();
-        interceptorAnnotations.addAll(roundEnv.getElementsAnnotatedWith(ProviderInterceptorPoint.class));
-        interceptorAnnotations.addAll(continuation.getInterceptorAnnotations());
+		/*
+		 * Do not assume that because the @ProviderInterceptorPoint annotation can only be applied to annotation types,
+		 * only TypeElements will be returned. Compilation errors on a class can let the compiler think the annotation
+		 * is applied to other elements even if it is correctly applied to a class, causing a class cast exception in
+		 * the for loop below.
+		 */
+		Set<Element> interceptorAnnotations = new HashSet<Element>();
+		interceptorAnnotations.addAll(roundEnv.getElementsAnnotatedWith(ProviderInterceptorPoint.class));
+		interceptorAnnotations.addAll(continuation.getInterceptorAnnotations());
 
-        mLogger.info(String.format("Start processing @%s annotations.", ProviderInterceptorPoint.class.getSimpleName()));
-        mLogger.trace(String.format("    Interfaces annotated with @%s for the round: %s",
-                ProviderInterceptorPoint.class.getSimpleName(), interceptorAnnotations));
+		mLogger.info(String.format("Start processing @%s annotations.", ProviderInterceptorPoint.class.getSimpleName()));
+		mLogger.trace(String.format("    Interfaces annotated with @%s for the round: %s",
+		        ProviderInterceptorPoint.class.getSimpleName(), interceptorAnnotations));
 
-        for (Element interceptorAnnotation : roundEnv.getElementsAnnotatedWith(ProviderInterceptorPoint.class)) {
+		for (Element interceptorAnnotation : roundEnv.getElementsAnnotatedWith(ProviderInterceptorPoint.class)) {
 
-            createAnnotationMetadata(interceptorAnnotation, metadata);
-        }
+			createAnnotationMetadata(interceptorAnnotation, metadata);
+		}
 
-        for (Element interceptorAnnotation : interceptorAnnotations) {
+		for (Element interceptorAnnotation : interceptorAnnotations) {
 
-            if (interceptorAnnotation instanceof TypeElement) {
+			if (interceptorAnnotation instanceof TypeElement) {
 
-                Set<? extends Element> elementsAnnotatedWithInterceptor = continuation.getElementsAnotatedWith(
-                        (TypeElement) interceptorAnnotation, roundEnv);
-                Set<TypeElement> interceptorClassSet = ElementFilter.typesIn(elementsAnnotatedWithInterceptor);
-                continuation.addInterceptorAnnotation((TypeElement) interceptorAnnotation);
-                continuation.addInterceptorClasses(interceptorClassSet);
+				Set<? extends Element> elementsAnnotatedWithInterceptor = continuation.getElementsAnotatedWith(
+				        (TypeElement) interceptorAnnotation, roundEnv);
+				Set<TypeElement> interceptorClassSet = ElementFilter.typesIn(elementsAnnotatedWithInterceptor);
+				continuation.addInterceptorAnnotation((TypeElement) interceptorAnnotation);
+				continuation.addInterceptorClasses(interceptorClassSet);
 
-                mLogger.trace(String.format("    Interceptor classes for %s: %s", interceptorAnnotation,
-                        interceptorClassSet));
+				mLogger.trace(String.format("    Interceptor classes for %s: %s", interceptorAnnotation,
+				        interceptorClassSet));
 
-                if (interceptorClassSet.size() > 1) {
-                    mLogger.trace(String.format(
-                            "    Multiple interceptors for annotation %s. Signaling compilatoin error.",
-                            interceptorAnnotation));
+				if (interceptorClassSet.size() > 1) {
+					mLogger.trace(String.format(
+					        "    Multiple interceptors for annotation %s. Signaling compilatoin error.",
+					        interceptorAnnotation));
 
-                    for (TypeElement interceptorClass : interceptorClassSet) {
+					for (TypeElement interceptorClass : interceptorClassSet) {
 
-                        mLogger.error(String.format("Only one interceptor class for annotation %s is supported."
-                                + " Found multiple interceptors: %s", interceptorAnnotation, interceptorClassSet),
-                                interceptorClass);
-                    }
+						mLogger.error(String.format("Only one interceptor class for annotation %s is supported."
+						        + " Found multiple interceptors: %s", interceptorAnnotation, interceptorClassSet),
+						        interceptorClass);
+					}
 
-                    continue;
-                }
+					continue;
+				}
 
-                if (interceptorClassSet.size() == 1) {
+				if (interceptorClassSet.size() == 1) {
 
-                    final TypeElement interceptorClass = interceptorClassSet.iterator().next();
+					final TypeElement interceptorClass = interceptorClassSet.iterator().next();
 
-                    processInterceptorAnnotation2(metadata, (TypeElement) interceptorAnnotation, interceptorClass);
-                }
-            }
-        }
+					processInterceptorAnnotation(metadata, (TypeElement) interceptorAnnotation, interceptorClass);
+				}
+			}
+		}
 
-        mLogger.info(String.format("Done processing @%s annotations.", ProviderInterceptorPoint.class.getSimpleName()));
-    }
+		mLogger.info(String.format("Done processing @%s annotations.", ProviderInterceptorPoint.class.getSimpleName()));
+	}
 
-    private void createAnnotationMetadata(Element interceptorAnnotation, Metadata metadata) {
+	private void createAnnotationMetadata(Element interceptorAnnotation, Metadata metadata) {
 
-        if (interceptorAnnotation instanceof TypeElement) {
+		if (interceptorAnnotation instanceof TypeElement) {
 
-            ConcreteAnnotation annotation = new ConcreteAnnotation((TypeElement) interceptorAnnotation);
+			ConcreteAnnotation annotation = new ConcreteAnnotation((TypeElement) interceptorAnnotation);
 
-            for (Element method : interceptorAnnotation.getEnclosedElements()) {
+			for (Element method : interceptorAnnotation.getEnclosedElements()) {
 
-                if (method instanceof ExecutableElement) {
-                    annotation.addAttribute(new AnnotationAttribute((ExecutableElement) method));
-                }
-            }
+				if (method instanceof ExecutableElement) {
+					annotation.addAttribute(new AnnotationAttribute((ExecutableElement) method));
+				}
+			}
 
-            metadata.registerConcreteAnnotation(annotation);
-        }
-    }
+			metadata.registerConcreteAnnotation(annotation);
+		}
+	}
 
-    private void processInterceptorAnnotation2(Metadata metadata, TypeElement interceptorAnnotation,
-            TypeElement interceptorClass) {
+	private void processInterceptorAnnotation(Metadata metadata, TypeElement interceptorAnnotation,
+	        TypeElement interceptorClass) {
 
-        for (DelegateClass delagateClass : metadata.getDelegateClasses()) {
+		for (DelegateClass delagateClass : metadata.getDelegateClasses()) {
 
-            for (DelegateMethod delegateMethod : delagateClass.getDelegateMethods()) {
+			for (DelegateMethod delegateMethod : delagateClass.getDelegateMethods()) {
 
-                mLogger.trace("    Processing method " + delegateMethod.getName());
-                ExecutableElement executableElement = delegateMethod.getExecutableElement();
+				mLogger.trace("    Processing method " + delegateMethod.getName());
+				ExecutableElement executableElement = delegateMethod.getExecutableElement();
 
-                List<? extends AnnotationMirror> annotationsMirrors = executableElement.getAnnotationMirrors();
+				List<? extends AnnotationMirror> annotationsMirrors = executableElement.getAnnotationMirrors();
 
-                for (AnnotationMirror mirror : annotationsMirrors) {
+				for (AnnotationMirror mirror : annotationsMirrors) {
 
-                    if (mTypeUtils.isSameType(mirror.getAnnotationType(), interceptorAnnotation.asType())) {
+					if (mTypeUtils.isSameType(mirror.getAnnotationType(), interceptorAnnotation.asType())) {
 
-                        Interceptor interceptor = new Interceptor(interceptorAnnotation, interceptorClass, mTypeUtils,
-                                metadata.getConcreteAnnotation(interceptorAnnotation));
+						Interceptor interceptor = createInterceptorWithAnnotationLiterals(metadata,
+						        interceptorAnnotation, interceptorClass, mirror);
 
-                        Map<? extends ExecutableElement, ? extends AnnotationValue> annotationValues = mElementUtils
-                                .getElementValuesWithDefaults(mirror);
+						delegateMethod.addInterceptor(interceptor);
+					}
+				}
 
-                        for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry : annotationValues
-                                .entrySet()) {
+				mLogger.trace("    Done processing method " + delegateMethod.getName());
+			}
+		}
+	}
 
-                            interceptor.addAnnotationValue(entry.getKey(), entry.getValue(), mElementUtils, mTypeUtils);
-                        }
+	private Interceptor createInterceptorWithAnnotationLiterals(Metadata metadata, TypeElement interceptorAnnotation,
+	        TypeElement interceptorClass, AnnotationMirror mirror) {
+		Interceptor interceptor = new Interceptor(interceptorAnnotation, interceptorClass, mTypeUtils,
+		        metadata.getConcreteAnnotation(interceptorAnnotation));
 
-                        delegateMethod.addInterceptor(interceptor);
-                    }
-                }
+		Map<? extends ExecutableElement, ? extends AnnotationValue> annotationValues = mElementUtils
+		        .getElementValuesWithDefaults(mirror);
 
-                mLogger.trace("    Done processing method " + delegateMethod.getName());
-            }
-        }
-    }
+		for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry : annotationValues.entrySet()) {
+
+			ExecutableElement attribute = entry.getKey();
+			AnnotationValue attributeValue = entry.getValue();
+
+			generateAnnotationLiteral(interceptor, attribute, attributeValue);
+		}
+
+		return interceptor;
+	}
+
+	private void generateAnnotationLiteral(Interceptor interceptor, ExecutableElement attribute,
+	        AnnotationValue attributeValue) {
+		final TypeKind kind = attribute.getReturnType().getKind();
+
+		switch (kind) {
+		case ARRAY:
+
+			generateAnnotationArrayLiteral(interceptor, attribute, attributeValue);
+
+			break;
+		case CHAR:
+
+			interceptor.addAnnotationValue(String.format("'%s'", attributeValue.getValue()));
+			break;
+		case FLOAT:
+
+			interceptor.addAnnotationValue(String.format("%sf", attributeValue.getValue()));
+			break;
+		case LONG:
+
+			interceptor.addAnnotationValue(String.format("%sL", attributeValue.getValue()));
+			break;
+
+		case DECLARED:
+
+			TypeElement stringType = mElementUtils.getTypeElement(String.class.getName());
+
+			if (mTypeUtils.isSameType(stringType.asType(), attribute.getReturnType())) {
+
+				interceptor.addAnnotationValue(String.format("\"%s\"", attributeValue.getValue()));
+			} else {
+
+				final Element asElement = mTypeUtils.asElement(attribute.getReturnType());
+
+				if (asElement.getKind() == ElementKind.ENUM) {
+					interceptor.addAnnotationValue(String.format("%s.%s", asElement, attributeValue.getValue()));
+				} else {
+
+					mLogger.error(String.format("Invalid type %s for the annotation attribute "
+					        + "%s; only primitive type, String and enumeration are permitted or 1-dimensional arrays"
+					        + " thereof.", asElement, attribute), attribute);
+				}
+			}
+
+			break;
+
+		default:
+			interceptor.addAnnotationValue(String.format("%s", attributeValue.getValue()));
+		}
+	}
+
+	private void generateAnnotationArrayLiteral(Interceptor interceptor, ExecutableElement attribute,
+	        AnnotationValue attributeValue) {
+		ArrayType typeMirror = (ArrayType) attribute.getReturnType();
+
+		switch (typeMirror.getComponentType().getKind()) {
+		case CHAR:
+
+			interceptor.addAnnotationValue(String.format("new char[] {%s}", attributeValue.getValue()));
+			break;
+		case FLOAT:
+
+			interceptor.addAnnotationValue(String.format("new float[] {%s}", attributeValue.getValue()));
+			break;
+		case DOUBLE:
+
+			interceptor.addAnnotationValue(String.format("new double[] {%s}", attributeValue.getValue()));
+			break;
+		case INT:
+
+			interceptor.addAnnotationValue(String.format("new int[] {%s}", attributeValue.getValue()));
+			break;
+		case LONG:
+
+			interceptor.addAnnotationValue(String.format("new long[] {%s}", attributeValue.getValue()));
+			break;
+
+		case DECLARED:
+
+			TypeElement stringType = mElementUtils.getTypeElement("java.lang.String");
+
+			if (mTypeUtils.isSameType(stringType.asType(), typeMirror.getComponentType())) {
+
+				interceptor.addAnnotationValue(String.format("new String[] {%s}", attributeValue.getValue()));
+			} else {
+
+				final Element asElement = mTypeUtils.asElement(typeMirror.getComponentType());
+
+				if (asElement.getKind() == ElementKind.ENUM) {
+					interceptor
+					        .addAnnotationValue(String.format("new %s[] {%s}", asElement, attributeValue.getValue()));
+				} else {
+
+					mLogger.error(String.format("Invalid type %s for the annotation attribute "
+					        + "%s; only primitive type, String and enumeration are permitted or 1-dimensional arrays"
+					        + " thereof.", asElement, attribute), attribute);
+				}
+			}
+
+			break;
+		default:
+			break;
+		}
+	}
 }
--- nudroid-annotations-processor/src/main/java/com/nudroid/annotation/processor/model/Interceptor.java
+++ nudroid-annotations-processor/src/main/java/com/nudroid/annotation/processor/model/Interceptor.java
@@ -4,16 +4,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import javax.lang.model.element.AnnotationValue;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.ArrayType;
-import javax.lang.model.type.TypeKind;
 import javax.lang.model.util.ElementFilter;
-import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
 
 /**
@@ -23,224 +17,128 @@ import javax.lang.model.util.Types;
  */
 public class Interceptor {
 
-    private TypeElement mInterceptorClassElement;
-    private TypeElement mInterceptorAnnotationElement;
-    private boolean mHasCustomConstructor;
-    private boolean mHasDefaultConstrucotr;
-    private ConcreteAnnotation mConcreteAnnotation;
-    private List<String> mConcreteAnnotationConstructorArguments = new ArrayList<String>();
+	private TypeElement mInterceptorClassElement;
+	private TypeElement mInterceptorAnnotationElement;
+	private boolean mHasCustomConstructor;
+	private boolean mHasDefaultConstrucotr;
+	private ConcreteAnnotation mConcreteAnnotation;
+	private List<String> mConcreteAnnotationConstructorArguments = new ArrayList<String>();
 
-    /**
-     * TODO correct javadoc Creates a new interceptor. TODO Remove the logic from the constructor and place it in the
-     * processor to avoid passing in the typeutils?
-     * 
-     * @param interceptorAnnotationElement
-     * @param interceptorClassElement
-     *            The type element for this interceptor.
-     * @param typeUtils
-     * @param mTypeUtils
-     */
-    public Interceptor(TypeElement interceptorAnnotationElement, TypeElement interceptorClassElement, Types typeUtils,
-            ConcreteAnnotation concreteAnnotation) {
+	/**
+	 * TODO correct javadoc Creates a new interceptor. TODO Remove the logic from the constructor and place it in the
+	 * processor to avoid passing in the typeutils?
+	 * 
+	 * @param interceptorAnnotationElement
+	 * @param interceptorClassElement
+	 *            The type element for this interceptor.
+	 * @param typeUtils
+	 * @param mTypeUtils
+	 */
+	public Interceptor(TypeElement interceptorAnnotationElement, TypeElement interceptorClassElement, Types typeUtils,
+	        ConcreteAnnotation concreteAnnotation) {
 
-        this.mInterceptorAnnotationElement = interceptorAnnotationElement;
-        this.mInterceptorClassElement = interceptorClassElement;
-        this.mConcreteAnnotation = concreteAnnotation;
+		this.mInterceptorAnnotationElement = interceptorAnnotationElement;
+		this.mInterceptorClassElement = interceptorClassElement;
+		this.mConcreteAnnotation = concreteAnnotation;
 
-        List<ExecutableElement> constructors = ElementFilter.constructorsIn(interceptorClassElement
-                .getEnclosedElements());
+		List<ExecutableElement> constructors = ElementFilter.constructorsIn(interceptorClassElement
+		        .getEnclosedElements());
 
-        for (ExecutableElement constructor : constructors) {
+		for (ExecutableElement constructor : constructors) {
 
-            final List<? extends VariableElement> parameters = constructor.getParameters();
+			final List<? extends VariableElement> parameters = constructor.getParameters();
 
-            if (parameters.size() == 0) {
-                this.mHasDefaultConstrucotr = true;
-            }
+			if (parameters.size() == 0) {
+				this.mHasDefaultConstrucotr = true;
+			}
 
-            if (parameters.size() == 1
-                    && typeUtils.isSameType(parameters.get(0).asType(), interceptorAnnotationElement.asType())) {
+			if (parameters.size() == 1
+			        && typeUtils.isSameType(parameters.get(0).asType(), interceptorAnnotationElement.asType())) {
 
-                this.mHasCustomConstructor = true;
-            }
-        }
-    }
+				this.mHasCustomConstructor = true;
+			}
+		}
+	}
 
-    /**
-     * Gets the fully qualified name of the interceptor class.
-     * 
-     * @return The fully qualified name of the interceptor class.
-     */
-    public String getQualifiedName() {
+	/**
+	 * Gets the fully qualified name of the interceptor class.
+	 * 
+	 * @return The fully qualified name of the interceptor class.
+	 */
+	public String getQualifiedName() {
 
-        return mInterceptorClassElement.getQualifiedName().toString();
-    }
+		return mInterceptorClassElement.getQualifiedName().toString();
+	}
 
-    /**
-     * Gets the simple name of the interceptor class.
-     * 
-     * @return The simple name of the interceptor class.
-     */
-    public String getSimpleName() {
+	/**
+	 * Gets the simple name of the interceptor class.
+	 * 
+	 * @return The simple name of the interceptor class.
+	 */
+	public String getSimpleName() {
 
-        return mInterceptorClassElement.getSimpleName().toString();
-    }
+		return mInterceptorClassElement.getSimpleName().toString();
+	}
 
-    /**
-     * Gets the annotation for this interceptor.
-     * 
-     * @return The annotation for this interceptor.
-     */
-    public TypeElement getInterceptorAnnotationElement() {
-        return mInterceptorAnnotationElement;
-    }
+	/**
+	 * Gets the annotation for this interceptor.
+	 * 
+	 * @return The annotation for this interceptor.
+	 */
+	public TypeElement getInterceptorAnnotationElement() {
+		return mInterceptorAnnotationElement;
+	}
 
-    /**
-     * TODO finish javadoc
-     * 
-     * @return the mHasCustomConstrucotr
-     */
-    public boolean hasDefaultConstructor() {
+	/**
+	 * TODO finish javadoc
+	 * 
+	 * @return the mHasCustomConstrucotr
+	 */
+	public boolean hasDefaultConstructor() {
 
-        return mHasDefaultConstrucotr;
-    }
+		return mHasDefaultConstrucotr;
+	}
 
-    /**
-     * TODO finish javadoc
-     * 
-     * @return the mHasCustomConstrucotr
-     */
-    public boolean hasCustomConstructor() {
+	/**
+	 * TODO finish javadoc
+	 * 
+	 * @return the mHasCustomConstrucotr
+	 */
+	public boolean hasCustomConstructor() {
 
-        return mHasCustomConstructor;
-    }
+		return mHasCustomConstructor;
+	}
 
-    /**
-     * @param attribute
-     * @param attributeValue
-     */
-    public void addAnnotationValue(ExecutableElement attribute, AnnotationValue attributeValue, Elements elementUtils,
-            Types typeUtils) {
+	/**
+	 * @param attribute
+	 * @param attributeValue
+	 */
+	public void addAnnotationValue(String value) {
 
-        final TypeKind kind = attribute.getReturnType().getKind();
+		mConcreteAnnotationConstructorArguments.add(value);
+	}
 
-        System.out.println("**** Type is " + kind);
-        System.out.println("**** Value is " + attributeValue);
+	/**
+	 * TODO Dinish javadoc
+	 * 
+	 * @return
+	 */
+	public String getConcreteAnnotationQualifiedName() {
 
-        switch (kind) {
-        case ARRAY:
+		return mConcreteAnnotation.getConcreteClassName();
+	}
 
-            ArrayType typeMirror = (ArrayType) attribute.getReturnType();
+	/**
+	 * TODO Finishs javadoc<br>
+	 * 
+	 * @return
+	 */
+	/*
+	 * Important. Velocity call this method each time it needs to iterate the array, creating a new unmodifiable
+	 * instance everytime. Change this here and in other methods.
+	 */
+	public List<String> getConcreteAnnotationConstructorArgumentLiterals() {
 
-            switch (typeMirror.getComponentType().getKind()) {
-            case CHAR:
-
-                mConcreteAnnotationConstructorArguments
-                        .add(String.format("new char[] {%s}", attributeValue.getValue()));
-                break;
-            case FLOAT:
-
-                mConcreteAnnotationConstructorArguments
-                        .add(String.format("new float[] {%s}", attributeValue.getValue()));
-                break;
-            case DOUBLE:
-
-                mConcreteAnnotationConstructorArguments.add(String.format("new double[] {%s}",
-                        attributeValue.getValue()));
-                break;
-            case INT:
-
-                mConcreteAnnotationConstructorArguments.add(String.format("new int[] {%s}", attributeValue.getValue()));
-                break;
-            case LONG:
-
-                mConcreteAnnotationConstructorArguments
-                        .add(String.format("new long[] {%s}", attributeValue.getValue()));
-                break;
-
-            case DECLARED:
-
-                TypeElement stringType = elementUtils.getTypeElement("java.lang.String");
-
-                if (typeUtils.isSameType(stringType.asType(), typeMirror.getComponentType())) {
-
-                    mConcreteAnnotationConstructorArguments.add(String.format("new String[] {%s}",
-                            attributeValue.getValue()));
-                } else {
-
-                    final Element asElement = typeUtils.asElement(typeMirror.getComponentType());
-
-                    if (asElement.getKind() == ElementKind.ENUM) {
-                        mConcreteAnnotationConstructorArguments.add(String.format("new %s[] {%s}", asElement,
-                                attributeValue.getValue()));
-                    }
-                }
-
-                break;
-            default:
-                break;
-            }
-
-            break;
-        case CHAR:
-
-            mConcreteAnnotationConstructorArguments.add(String.format("'%s'", attributeValue.getValue()));
-            break;
-        case FLOAT:
-
-            mConcreteAnnotationConstructorArguments.add(String.format("%sf", attributeValue.getValue()));
-            break;
-        case LONG:
-
-            mConcreteAnnotationConstructorArguments.add(String.format("%sL", attributeValue.getValue()));
-            break;
-
-        case DECLARED:
-
-            TypeElement stringType = elementUtils.getTypeElement(String.class.getName());
-
-            if (typeUtils.isSameType(stringType.asType(), attribute.getReturnType())) {
-
-                mConcreteAnnotationConstructorArguments.add(String.format("\"%s\"", attributeValue.getValue()));
-            } else {
-
-                final Element asElement = typeUtils.asElement(attribute.getReturnType());
-
-                if (asElement.getKind() == ElementKind.ENUM) {
-                    mConcreteAnnotationConstructorArguments.add(String.format("%s.%s", asElement,
-                            attributeValue.getValue()));
-                }
-            }
-
-            break;
-
-        default:
-            mConcreteAnnotationConstructorArguments.add(String.format("%s", attributeValue.getValue()));
-        }
-    }
-
-    /**
-     * TODO Dinish javadoc
-     * 
-     * @return
-     */
-    public String getConcreteAnnotationQualifiedName() {
-
-        return mConcreteAnnotation.getConcreteClassName();
-    }
-
-    /**
-     * TODO Finishs javadoc<br>
-     * 
-     * @return
-     */
-    /*
-     * Important. Velocity call this method each time it needs to iterate the array, creating a new unmodifiable
-     * instance everytime. Change this here and in other methods.
-     */
-    public List<String> getConcreteAnnotationConstructorArgumentLiterals() {
-
-        System.out.println(mConcreteAnnotationConstructorArguments);
-        return Collections.unmodifiableList(mConcreteAnnotationConstructorArguments);
-    }
+		return Collections.unmodifiableList(mConcreteAnnotationConstructorArguments);
+	}
 }
