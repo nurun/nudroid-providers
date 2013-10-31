581a4390f2401690c5ec3f530e9d273f22fa524c
Daniel Freitas <daniel.freitas@nurun.com>
2013-10-31 11:41:12 -0400

    Updated documentation on ProviderInterceptorPoint annotation

--- nudroid-annotations/src/main/java/com/nudroid/annotation/provider/interceptor/ProviderInterceptorPoint.java
+++ nudroid-annotations/src/main/java/com/nudroid/annotation/provider/interceptor/ProviderInterceptorPoint.java
@@ -6,71 +6,67 @@ import java.lang.annotation.Target;
 /**
  * Flags an annotation as an interceptor point for content provider delegates.
  * <p/>
- * Annotations annotated with {@link ProviderInterceptorPoint} can be used to provide implementations of content
- * provider interceptors as well as applying those interceptors to delegate methods. Here's an example usage:
+ * Annotations annotated with {@link ProviderInterceptorPoint} can be used to apply interceptors to content provider
+ * delegate methods. Interceptor annotations must be defined as a static inner class of the corresponding interceptor
+ * class.
+ * <p/>
+ * Interceptor classes must provide either one of a default constructor or a constructor with a
+ * {@link ProviderInterceptorPoint} annotation as a parameter (see example below for more details).
+ * <p/>
+ * Here's an example usage:
  * <p/>
  * 
  * <pre>
- * //#Creates the interceptor point called Log
- * //##################################
- * &#064;ProviderInterceptorPoint
- * public &#064;interface Log {
- *     String level() default &quot;&quot;;
- * }
- * 
- * //Implementes the log interceptor
+ * //Create an interceptor for logging
  * //################################
- * import android.content.ContentValues;
  * import android.database.Cursor;
- * import android.net.Uri;
  * 
+ * import com.nudroid.provider.delegate.ContentProviderContext;
  * import com.nudroid.provider.interceptor.ContentProviderInterceptor;
+ * import com.nudroid.annotation.provider.interceptor.ProviderInterceptorPoint;
  * 
- * &#064;Log
  * public class LogInterceptor implements ContentProviderInterceptor {
  * 
- *     public void beforeQuery(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
- *         // Logs the event.
+ *     private String mLogLevel;
+ * 
+ *     public void beforeQuery(ContentProviderContext contentProviderContext) {
+ *     
+ *         // Logs check log level and log the event.
  *     }
  * 
- *     public Cursor afterQuery(Cursor result, Uri uri, String[] projection, String selection, String[] selectionArgs,
- *             String sortOrder) {
- *         //Replace result by a decorated cursor with added functionalities
- *         //This demonstrates how the results from the delegate method can be changed before being returned.
- *         return new PotentialNewCursor(result);
+ *     public Cursor afterQuery(ContentProviderContext contentProviderContext, Cursor result) {
+ *     
+ *         // Don't change the result
+ *         return result;
  *     }
  * 
- *     public void beforeUpdate(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
- *         ...
- *     }
+ *     ...
+ * }
  * 
- *     public int afterUpdate(int result, Uri uri, ContentValues values, String selection, String[] selectionArgs) {
- *         ...
- *     }
+ * //#Create the ProviderInterceptorPoint annotation
+ * //#and interceptor constructor (optional)
+ * //##################################
+ * import android.database.Cursor;
  * 
- *     public void beforeInsert(Uri uri, ContentValues values) {
- *         ...
- *     }
+ * import com.nudroid.provider.delegate.ContentProviderContext;
+ * import com.nudroid.provider.interceptor.ContentProviderInterceptor;
+ * import com.nudroid.annotation.provider.interceptor.ProviderInterceptorPoint;
  * 
- *     public Uri afterInsert(Uri result, Uri uri, ContentValues values) {
- *         ...
- *     }
+ * public class LogInterceptor implements ContentProviderInterceptor {
  * 
- *     public void beforeDelete(Uri uri, String selection, String[] selectionArgs) {
- *         ...
- *     }
+ *     String mLogLevel;
  * 
- *     public int afterDelete(int result, Uri uri, String selection, String[] selectionArgs) {
- *         ...
+ *     public LogInterceptor(Log log) {
+ *     
+ *         this.mLogLevel = log.level();
  *     }
- * 
- *     public void beforeGetType(Uri uri) {
- *         ...
- *     }
- * 
- *     public String afterGetType(String result, Uri uri) {
- *         ...
- *     }
+ *     
+ *     ...
+ *     
+ *     &#064;ProviderInterceptorPoint
+ *     public &#064;interface Log {
+ *         String level() default &quot;TRACE&quot;;
+ *     }     
  * }
  * 
  * //#Apply the Log interceptor to a delegate method
@@ -78,23 +74,23 @@ import java.lang.annotation.Target;
  * &#064;ContentProviderDelegate(authority = "authority")
  * public class ProviderDelegate {
  * 
- *     &#064;Log
+ *     &#064;LogInterceptor.Log(level = "INFO")
  *     &#064;Query("/users/{id}")
  *     public Cursor listAllUsers(@Projection String[] projection, @PathParam("id") String userId) {
  * 
  *         ...
  *     }
  * }
- * 
  * </pre>
  * 
  * The query delegate method annotated with the Log annotation will then be intercepted by the LogInterceptor which will
  * execute the relevant before and after methods around listAllUsers().
  * <p/>
  * <tt>beforeXXX</tt> methods will receive all parameters that will be passed to the delegate method. The interceptor
- * can inspect and even change these parameters and the changes will be carried over to the delegate method. Similarly,
- * <tt>afterXXX</tt> methods will receive these parameters plus the result returned by the delegate method. The
- * interceptor can change this result by returning another object from the <tt>afterXXX</tt> methods.
+ * can inspect and even change these parameters and the changes will be carried over to the delegate method and
+ * subsequent interceptors. Similarly, <tt>afterXXX</tt> methods will receive these parameters plus the result returned
+ * by the delegate method. The interceptor can change this result by returning another object from the <tt>afterXXX</tt>
+ * methods.
  * 
  * @author <a href="mailto:daniel.mfreitas@gmail.com">Daniel Freitas</a>
  */
